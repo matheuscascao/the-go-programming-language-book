e7d72a0f62a27b72ba567c575d9c5937d5b24d62
matheuscascao <matheusfranco23@hotmail.com>
2024-04-24 22:46:48 -0300

    first commit

---
+++ ch1/dup2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	}
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+		}
+		countLines(f, counts)
+		f.Close()
+
+		for line, n := range counts {
+			fmt.Printf("%d\t%s\n", n, line)
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
