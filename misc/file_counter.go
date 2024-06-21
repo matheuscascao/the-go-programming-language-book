e022b471eee018b7e229070c5e41a80f76f4e70c
matheuscascao <matheusfranco23@hotmail.com>
2024-06-20 22:16:21 -0300

    count file

--- misc/file_counter.go
+++ misc/file_counter.go
@@ -4,22 +4,37 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
-	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
 
-	arg := os.Args[3]
-
-	fmt.Println(argsWithProg)
 	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
 
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
+
 	exePath := filepath.Dir(ex)
 	fmt.Println(exePath)
+
+	files, err := os.ReadDir(exePath)
+	if err != nil {
+		panic(err)
+	}
+	
+	mapFileCount := make(map[string]int)
+
+	for _, file := range files {
+		// fmt.Println(file.Name(), file.IsDir())
+		fileName := file.Name()
+		splitedFileName := strings.Split(fileName, ".")
+		fileExtension := splitedFileName[len(splitedFileName)-1]
+
+		mapFileCount[fileExtension] = mapFileCount[fileExtension]+1
+	}
+
+	fmt.Println(mapFileCount)
 }
